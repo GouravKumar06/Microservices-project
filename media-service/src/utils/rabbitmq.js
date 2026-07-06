5405cd74376cc5c06be30a65d4bf41608c2472bb
GouravKumar06 <gka98963@gmail.com>
2026-07-06 11:03:32 +0530

    Resolve rabbit mq connection issue

--- media-service/src/utils/rabbitmq.js
+++ media-service/src/utils/rabbitmq.js
@@ -7,35 +7,39 @@ let channel = null;
 
 const EXCHANGE_NAME = "post_events";
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 exports.connectRabbitMQ = async () => {
-    try {
-        connection = await amqp.connect(process.env.RABBITMQ_URL);
 
-        channel = await connection.createChannel();
+    while (true) {
 
-        await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false }); // durable: false means the exchange won't survive a broker restart
-        logger.info("Connected to RabbitMQ");
+        try {
 
-        return channel;
-    } catch (error) {
-        logger.error("Error connecting to RabbitMQ", error);
+            logger.info("Connecting to RabbitMQ...");
+
+            connection = await amqp.connect(process.env.RABBITMQ_URL);
+
+            channel = await connection.createChannel();
+
+            await channel.assertExchange(
+                EXCHANGE_NAME,
+                "topic",
+                { durable: false }
+            );
+
+            logger.info("RabbitMQ Connected");
+
+            break;
+
+        } catch (err) {
+
+            logger.error("RabbitMQ not ready. Retrying in 5 seconds...");
+
+            await sleep(5000);
+
+        }
     }
-}
-
-
-// exports.publishEvent = async(routingKey,message) => {
-//     try{
-//         if(!channel){
-//             logger.error("RabbitMQ channel is not established. Cannot publish event.");
-//             return;
-//         }
-//         await channel.publish(EXCHANGE_NAME, routingKey, Buffer.from(JSON.stringify(message)));
-//         logger.info(`Event published to RabbitMQ with routing key: ${routingKey}`);
-//     }catch(error){
-//         logger.error("Error publishing event to RabbitMQ", error);
-//     }
-// }
+};
 
 exports.consumeEvent = async(routingKey,callback) => {
     try{
